7e64e70608d9b946b6a98a60f3889127b0975452
Farhan <farhan.kzsr23@gmail.com>
2021-06-27 17:02:02 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meonk Books',
+      theme: ThemeData(),
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buku Meonk'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('This is drawer header'),
+            ),
+            ListTile(
+              title: Text('item 1'),
+              onTap: () {
+                //where should it go
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('item 2'),
+              onTap: () {
+                //where should it go
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('item 2'),
+              onTap: () {
+                //where should it go
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.remove,
+        overlayColor: Colors.white,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        renderOverlay: false,
+        overlayOpacity: 0.5,
+        visible: true,
+        closeManually: false,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.accessibility),
+            backgroundColor: Colors.red,
+            label: 'First',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('FIRST CHILD'),
+            onLongPress: () => print('FIRST CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.brush),
+            backgroundColor: Colors.blue,
+            label: 'Second',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('SECOND CHILD'),
+            onLongPress: () => print('SECOND CHILD LONG PRESS'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.keyboard_voice),
+            backgroundColor: Colors.green,
+            label: 'Third',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () => print('THIRD CHILD'),
+            onLongPress: () => print('THIRD CHILD LONG PRESS'),
+          ),
+        ],
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return MainMenu();
+        },
+      ),
+    );
+  }
+}
+
+class MainMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {},
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                    flex: 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        'images/1.jpg',
+                        height: 200,
+                      ),
+                    )),
+                Expanded(
+                    flex: 3,
+                    child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Judul Buku',
+                              style: TextStyle(
+                                  fontSize: 26, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              'Author',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text('Rating'),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+                          ],
+                        ))),
+                Expanded(
+                  flex: 1,
+                  child: Bookmarked(),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Bookmarked extends StatefulWidget {
+  @override
+  _BookmarkedState createState() => _BookmarkedState();
+}
+
+class _BookmarkedState extends State<Bookmarked> {
+  bool isMarked = false;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          isMarked = !isMarked;
+        });
+      },
+      icon: Icon(
+        isMarked ? Icons.bookmark : Icons.bookmark_outline,
+        size: 50,
+      ),
+    );
+  }
+}
