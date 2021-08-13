562bb52713eb651a1017e795e4b48b61d31ed725
Farhan <farhan.kzsr23@gmail.com>
2021-08-13 10:58:12 +0700

    now search func work as intended

--- lib/searchScreen.dart
+++ lib/searchScreen.dart
@@ -77,6 +77,7 @@ class _AppBarDesignState extends State<SearchBar> {
           color: global.isDarkModeEnabled ? Colors.white : Colors.black,
           fontSize: 20),
       onChanged: (query) {
+        queryList.clear();
         if (query.isNotEmpty) {
           for (int i = 0; i < bookList.length; i++) {
             if (bookList[i].title.toLowerCase().contains(query.toLowerCase())) {
@@ -84,6 +85,9 @@ class _AppBarDesignState extends State<SearchBar> {
               runApp(MyApp());
             }
           }
+        }
+        if (query.isEmpty) {
+          queryList.clear();
         }
       },
     );
