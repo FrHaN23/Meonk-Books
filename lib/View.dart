bc8a89b009fb5dabaae84f6a933e54ccb65fe8e8
Farhan <farhan.kzsr23@gmail.com>
2021-08-13 10:44:25 +0700

    remmove unused comment

--- lib/View.dart
+++ lib/View.dart
@@ -136,7 +136,6 @@ class _ListViewMainState extends State<ListViewMain> {
                                 )
                               ],
                             ),
-
                             SizedBox(
                               height: 10,
                             ),
@@ -156,9 +155,6 @@ class _ListViewMainState extends State<ListViewMain> {
                                         : 16,
                               ),
                             ),
-
-                            //Text(
-                            //    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
                           ],
                         ),
                       ),
